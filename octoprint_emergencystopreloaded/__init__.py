329e2f9f8839ac7a323b92457775aa92a2b73c53
C.M.Rasch <65172002+CMR-DEV@users.noreply.github.com>
2022-07-01 20:34:20 +0200

    Add GPIO_MODE enum to __init__.py

--- octoprint_emergencystopreloaded/__init__.py
+++ octoprint_emergencystopreloaded/__init__.py
@@ -7,7 +7,11 @@ from octoprint.events import Events
 from time import sleep
 import RPi.GPIO as GPIO
 import flask
+from enum import IntEnum
 
+class GPIO_MODE( IntEnum ):
+    BOARD   = 10
+    BCM     = 11
 
 class EmergencyStopReloadedPlugin(
     octoprint.plugin.StartupPlugin,
@@ -23,10 +27,10 @@ class EmergencyStopReloadedPlugin(
 
     # gpio mode set by 3rd party
     gpio_mode_disabled  = False
-    
+
     # printing flag
     printing            = False
-    
+
     # whether or not the gcode has already been sent
     gcode_sent          = False
 
@@ -84,12 +88,12 @@ class EmergencyStopReloadedPlugin(
     # Settings hook
     def get_settings_defaults( self ):
         return {
-            "gpio_mode":            10,
+            "gpio_mode":            GPIO_MODE.BOARD.value,
             "pin":                  0,
             "power":                0,
             "g_code":               self.default_gcode,
             "triggered ":           0,
-            
+
             "bounce_time":          250,
             "reading_iterations":   5,
             "reading_delay":        100,
@@ -98,16 +102,16 @@ class EmergencyStopReloadedPlugin(
     @octoprint.plugin.BlueprintPlugin.route( "/state", methods=[ "GET" ] )
     def on_api_get_state( self ):
         self._logger.debug("getting state info")
-        
+
         return flask.jsonify( printing=self.printing, gpio_mode_disabled=self.gpio_mode_disabled )
-        
+
     # simpleApiPlugin
     def get_api_commands( self ):
         return { "testSensor": [ "pin", "power" ] }
-        
+
     # test pin value, power pin or if its used by someone else
     def on_api_command( self, command, data ):
-        
+
         try:
             selected_power          = int( data.get("power") )
             selected_pin            = int( data.get("pin") )
@@ -116,7 +120,7 @@ class EmergencyStopReloadedPlugin(
 
             if selected_pin is 0:
                 return "", 556
-            
+
             # init gpios with the test values:
             self.init_gpio(
                 selected_mode,
@@ -125,14 +129,14 @@ class EmergencyStopReloadedPlugin(
                 selected_switch_type,
                 True
             )
-            
+
             # take measurement:
             triggered = self.read_sensor_multiple(
                 selected_pin,
                 selected_power,
                 selected_switch_type
             )
-            
+
             # restore gpio setup with the values from the plugin settings:
             self.init_gpio(
                 self.setting_gpio_mode,
@@ -141,20 +145,20 @@ class EmergencyStopReloadedPlugin(
                 self.setting_triggered,
                 True
             )
-            
+
             return flask.jsonify( triggered=triggered )
-            
+
         except ValueError as e:
             self._logger.error( str(e) )
             # ValueError occurs when reading from power, ground or out of range pins
             return "", 556
-    
+
     def send_gcode( self, msg ):
-        
+
         self._logger.info("Sending GCODE: %s" % self.setting_gcode )
         self._printer.commands( self.setting_gcode )
         self.gcode_sent = True
-    
+
         self._plugin_manager.send_plugin_message(
             self._identifier,
             {
@@ -163,48 +167,48 @@ class EmergencyStopReloadedPlugin(
                 "msg":          str( msg )
             }
         )
-    
+
     def sensor_callback( self, _ ):
-    
+
         self._logger.info( "Sensor callback called" )
-        
+
         if self.read_sensor_multiple( self.setting_pin, self.setting_power, self.setting_triggered ):
-            
+
             if not self.gcode_sent:
                 self.send_gcode( "Triggered!" )
-        
+
         else:
             self.gcode_sent = False
 
     def init_gpio( self, gpio_mode, pin, power, trigger_mode, test ):
         self._logger.info( "Initializing GPIO" )
-        
+
         preset_gpio_mode = GPIO.getmode()
-        
+
         if preset_gpio_mode is not None:
             self.gpio_mode_disabled = True
             gpio_mode               = preset_gpio_mode
             self._settings.set( [ "gpio_mode" ], preset_gpio_mode )
-            
+
         else:
             self._logger.info( "Preset mode is %s" % preset_gpio_mode )
 
         if self.plugin_enabled( pin ):
-        
+
             self._logger.info( "Enabling emergency stop sensor" )
-            
+
             # BOARD
-            if gpio_mode is 10:
-            
+            if gpio_mode == GPIO_MODE.BCM:
+
                 self._logger.info( "Mode is 10 (BOARD)" )
-                
-                # if mode set by 3rd party don't set it again                
+
+                # if mode set by 3rd party don't set it again
                 if not self.gpio_mode_disabled:
                     self._logger.info( "Setting Board mode" )
                     GPIO.cleanup()
-                        
+
                     GPIO.setmode( GPIO.BOARD )
-                    
+
                 # first check pins not in use already
                 usage = GPIO.gpio_function( pin )
                 self._logger.debug( "Usage on pin %s is %s" % ( pin, usage ) )
@@ -212,70 +216,70 @@ class EmergencyStopReloadedPlugin(
                 if usage is not 1:
                     # 555 is not http specific so I chose it
                     return "", 555
-                    
+
             # BCM
-            elif gpio_mode is 11:
-            
+            elif gpio_mode == GPIO_MODE.BCM:
+
                 self._logger.info( "Mode is 11 (BCM)" )
-                
+
                 # BCM range 1-27
                 if pin > 27:
                     return "", 556
-                    
+
                 # if mode set by 3rd party don't set it again
                 if not self.gpio_mode_disabled:
                     self._logger.debug( "Setting BCM mode" )
                     GPIO.cleanup()
-                        
+
                     GPIO.setmode( GPIO.BCM )
-            
+
             # attach event listener ( = self.sensor_callback ) when method is not called for testing:
             if not test:
                 try:
                     # 0 = sensor is grounded, react to rising edge pulled up by pull up resistor
                     if power is 0:
-                    
+
                         self.pull_resistor( pin, power )
-                        
+
                         # triggered when open
                         if trigger_mode is 0:
                             self._logger.debug( "Reacting to rising edge" )
                             GPIO.add_event_detect(
-                            
+
                                 pin,
                                 GPIO.RISING,
                                 callback=self.sensor_callback,
                                 bouncetime=self.setting_bounce_time
-                                
+
                             )
-                            
+
                         # triggered when closed
                         else:
                             self._logger.debug( "Reacting to falling edge" )
                             GPIO.add_event_detect(
-                            
+
                                 pin,
                                 GPIO.FALLING,
                                 callback=self.sensor_callback,
                                 bouncetime=self.setting_bounce_time
-                                
+
                             )
 
                     # 1 = sensor is powered, react to falling edge pulled down by pull down resistor
                     else:
-                    
+
                         self.pull_resistor( pin, power )
-                        
+
                         # triggered when open
                         if trigger_mode is 0:
                             self._logger.debug( "Reacting to falling edge" )
                             GPIO.add_event_detect(
-                            
+
                                 pin,
                                 GPIO.FALLING,
                                 callback=self.sensor_callback,
                                 bouncetime=self.bounce_time
-                                
+
                             )
                         # triggered when closed
                         else:
@@ -284,7 +288,7 @@ class EmergencyStopReloadedPlugin(
                                 pin, GPIO.RISING,
                                 callback=self.sensor_callback,
                                 bouncetime=self.bounce_time)
-                                
+
                 except RuntimeError as e:
                     self._logger.warn( str( e ) )
         else:
@@ -295,16 +299,16 @@ class EmergencyStopReloadedPlugin(
         if power is 0:
             # self._logger.debug("Pulling up resistor")
             GPIO.setup( pin, GPIO.IN, pull_up_down=GPIO.PUD_UP )
-            
+
         elif power is 1:
             # self._logger.debug("Pulling down resistor")
             GPIO.setup( pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
-            
+
         # self._logger.debug("Done")
 
     def on_after_startup( self ):
         self._logger.info( "Emergency Stop Reloaded started" )
-        
+
         self.init_gpio(
             self.setting_gpio_mode,
             self.setting_pin,
@@ -312,15 +316,17 @@ class EmergencyStopReloadedPlugin(
             self.setting_triggered,
             False
         )
-        
+
         self.initialized = True
+
+        self._logger.info( "Reading sensor due to startup" )
         self.sensor_callback( None ) # inital read
 
     def on_settings_save( self, data ):
         # Retrieve any settings not changed in order to validate that the combination of new and old settings end up in a bad combination
-        
+
         self._logger.info( "Saving settings for Emergency Stop Reloaded" )
-        
+
         pin_to_save             = self._settings.get_int(["pin"])
         gpio_mode_to_save       = self._settings.get_int(["gpio_mode"])
         power_to_save           = self._settings.get_int(["power"])
@@ -331,16 +337,16 @@ class EmergencyStopReloadedPlugin(
             "gpio_mode",
             "power",
             "trigger",
-            
+
             "bounce_time",
             "reading_iterations",
             "reading_delay",
         ]:
             if key in data:
                 value = data.get( key )
-                
+
                 if not value.isdigit():
-                
+
                     self._plugin_manager.send_plugin_message(
                         self._identifier,
                         {
@@ -349,13 +355,13 @@ class EmergencyStopReloadedPlugin(
                             "msg":          "Emergency stop settings not saved: " + key + " is not an integer"
                         }
                     )
-                    
-                    return 
-                
+
+                    return
+
                 intVal = int( value )
-                
+
                 if key is "reading_iterations" and intVal < 1:
-                
+
                     self._plugin_manager.send_plugin_message(
                         self._identifier,
                         {
@@ -364,11 +370,11 @@ class EmergencyStopReloadedPlugin(
                             "msg":          "Emergency stop settings not saved: " + key + " must be positive"
                         }
                     )
-                    
-                    return 
-                
+
+                    return
+
                 elif key in ( "bounce_time", "reading_delay" ) and intVal < 0:
-                
+
                     self._plugin_manager.send_plugin_message(
                         self._identifier,
                         {
@@ -377,9 +383,9 @@ class EmergencyStopReloadedPlugin(
                             "msg":          "Emergency stop settings not saved: " + key + " must be non-negative"
                         }
                     )
-                    
-                    return 
-            
+
+                    return
+
         if "pin" in data:
             pin_to_save             = int( data.get("pin") )
 
@@ -391,20 +397,20 @@ class EmergencyStopReloadedPlugin(
 
         if "trigger" in data:
             trigger_mode_to_save    = int( data.get("triggered") )
-        
+
         # pin validation:
         if pin_to_save is not None:
             # check if pin is not power/ground pin or out of range but allow the disabled value (0)
             if pin_to_save is not 0:
                 try:
                     # BOARD
-                    if gpio_mode_to_save is 10:
+                    if gpio_mode_to_save == GPIO_MODE.BCM:
                         # before saving check if pin not used by others
                         usage = GPIO.gpio_function( pin_to_save )
                         self._logger.debug("usage on pin %s is %s" % ( pin_to_save, usage ))
-                        
+
                         if usage is not 1:
-                            self._logger.info( 
+                            self._logger.info(
                                 "You are trying to save pin %s which is already used by others" % pin_to_save )
                             self._plugin_manager.send_plugin_message(
                                 self._identifier,
@@ -416,8 +422,8 @@ class EmergencyStopReloadedPlugin(
                             )
                             return
                     # BCM
-                    elif gpio_mode_to_save is 11:
-                    
+                    elif gpio_mode_to_save == GPIO_MODE.BCM:
+
                         if pin_to_save > 27:
                             self._logger.info(
                                 "You are trying to save pin %s which is out of range" % pin_to_save)
@@ -440,9 +446,9 @@ class EmergencyStopReloadedPlugin(
                             "msg":          "Emergency stop settings not saved, you are trying to use a pin which is ground/power pin or out of range"
                         }
                     )
-                            
+
                     return
-                    
+
                 self.init_gpio(
                     gpio_mode_to_save,
                     pin_to_save,
@@ -450,51 +456,51 @@ class EmergencyStopReloadedPlugin(
                     trigger_mode_to_save,
                     False
                 )
-                
+
         octoprint.plugin.SettingsPlugin.on_settings_save( self, data )
 
     def read_sensor_multiple( self, pin, power, trigger_mode ):
-    
+
         self._logger.info( "Reading sensor values %s times from pin %s" % ( self.setting_reading_iterations, pin ) )
-        
+
         prevVal = None
         i       = 0
-        
+
         # take a reading of multiple consecutive reads to prevent false positives
         while True:
             curentVal = self.read_sensor( pin, power, trigger_mode )
             i += 1
-            
+
             if prevVal is None:
                 prevVal = curentVal
-                
+
             elif prevVal != curentVal:
                 i = 0
                 self._logger.info( "Repeating sensor read due to false positives" )
-                
+
             if i < self.setting_reading_iterations:
                 sleep( self.setting_reading_delay / 1000 )
-                
+
             else:
-            
-                self._logger.info( "Reading result: %s" % curentVal )        
+
+                self._logger.info( "Reading result: %s" % curentVal )
                 return curentVal
 
     # read sensor input value
     def read_sensor( self, pin, power, trigger_mode ):
-    
+
         # self._logger.debug("Reading pin %s " % pin)
-        
+
         self.pull_resistor( pin, power )
-        
+
         return ( GPIO.input( pin ) + power + trigger_mode ) % 2 is 0
 
     # plugin disabled if pin set to 0
     def plugin_enabled( self, pin ):
         return pin >= 0
-        
+
     def on_event( self, event, payload ):
-        
+
         if event in (
             Events.HOME,
             Events.Z_CHANGE,
@@ -502,38 +508,38 @@ class EmergencyStopReloadedPlugin(
             Events.PRINT_STARTED,
             Events.PRINT_RESUMED
         ):
-        
+
             self.gcode_sent = False
-        
+
             if event in ( Events.PRINT_STARTED, Events.PRINT_RESUMED ):
                 self.printing = True
-            
+
             # ignore z_change when printing
             if event is not Events.Z_CHANGE or not self.printing:
-            
+
                 if self.initialized and self.plugin_enabled( self.setting_pin ):
-                        
+
                     self._logger.debug("Reading sensor due to event: " + event)
-                        
+
                     if self.read_sensor_multiple( self.setting_pin, self.setting_power, self.setting_triggered ):
                         self.send_gcode("Emergency stop is still active! Please reset!")
-        
+
         elif event is Events.CLIENT_OPENED:
-        
+
             # if the plugin hasn't been configured
             if not self.plugin_enabled( self.setting_pin ):
-            
+
                 self._plugin_manager.send_plugin_message(
-                
+
                     self._identifier,
                     dict(
                         type        = "info",
                         autoClose   = True,
                         msg         = "Don't forget to configure this plugin."
                     )
-                    
+
                 )
-            
+
         elif event in (
             Events.PRINT_DONE,
             Events.PRINT_FAILED,
@@ -541,24 +547,24 @@ class EmergencyStopReloadedPlugin(
             Events.ERROR,
             Events.E_STOP
         ):
-            
+
             self._logger.debug("Resetting due to event: " + event)
             self.gcode_sent = False
             self.printing   = False
-            
+
         # else:
             # self._logger.debug( "Event: " + event )
 
     def get_update_information( self ):
-    
+
         # Define the configuration for your plugin to use with the Software Update
         # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
         # for details.
-        
+
         return {
-        
+
             "emergencystopreloaded": {
-            
+
                 "displayName":      "Emergency Stop Reloaded",
                 "displayVersion":   self._plugin_version,
 
@@ -570,9 +576,9 @@ class EmergencyStopReloadedPlugin(
 
                 # update method: pip
                 "pip":              "https://github.com/CMR-DEV/OctoPrint-EmergencyStopReloaded/archive/{target_version}.zip"
-                
+
             }
-            
+
         }
 
 
@@ -591,13 +597,13 @@ def __plugin_check__():
 
     try:
         import RPi.GPIO as GPIO
-        
+
         if GPIO.VERSION < "0.6":  # Need at least 0.6 for edge detection
             return False
-            
+
     except ImportError:
         return False
-        
+
     return True
 
 
